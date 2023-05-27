9844b682fb7ec6734a9600ae8902b43100aeacdb
Quoc Pham <quoc_pham@msn.com>
2023-05-27 12:37:22 -0500

    Added upkeep and fixed bugs.

---
+++ Hmt.Common.Core/Views/ConsoleMessage.cs
@@ -0,0 +1,15 @@
+﻿namespace Hmt.Common.Core.Views;
+
+public class ConsoleMessage
+{
+    public ConsoleMessage(string message) : this(message, ConsoleColor.Gray) { }
+
+    public ConsoleMessage(string message, ConsoleColor color)
+    {
+        Message = message;
+        Color = color;
+    }
+
+    public string Message { get; set; } = string.Empty;
+    public ConsoleColor Color { get; set; }
+}
